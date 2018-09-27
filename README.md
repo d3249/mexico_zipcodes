66cfefccdc6b170dab1d97777aa169e591977ad4
Edgar Ramos <edgar.a.ramos.rambaud@gmail.com>
2018-09-27 16:01:24 -0500

    Changes database structure
    Zipcode get inlined in the asentamiento (settlement) table. A corresponding
    index is generated for fast search.

--- README.md
+++ README.md
@@ -36,9 +36,9 @@ script CODIGOS_POSTALES.sql
 
 > El script generado tarda 3+ horas en ejecturase en una computadora Acer aspire A-515 con MariaDB en Arch Linux
 
-**Pendientes**:
-    * Separar los scripts de estructura y de insersión de datos
-    * Optimizar el script
+**Pendientes**:  
+  * Separar los scripts de estructura y de insersión de datos
+  * Optimizar el script
 
 Si vas a usar esta base de datos, considera descargar el archivo diréctamente del sitio para que esté actualizado.
 
@@ -75,8 +75,9 @@ With this, the script generates the basic catalogs (Settlement type, City, State
 
 > Generated script takes 3+ hours to run on  Acer aspire A-515 con MariaDB en Arch Linux
 
-**TODO**:
-    * Split structure and inserts scripts
-    * Cleanup the script code
+**TODO**:  
+  * Split structure and inserts scripts
+  * Cleanup the script code
 
 If you're using this data base, consider to download the file from the site in order to be updated.
+jkk
--- zipcodes.rb
+++ zipcodes.rb
@@ -43,27 +43,85 @@ module Zipcodes
     
     def self.convert file
 
+        read_file file
+
+        print_summary_info
+
+        write_output_file
+        
+    end
+
+    def read_file file
         file_object = file_iterator file
         read_lines file_object
+    end
 
+    def file_iterator file
+        puts "Loading file #{file}"
+        File.open(file,"r:#{@@input_encoding}")
+    end
+
+    def read_lines file
+        lasts = ['','','','','']
+        file.each_line do |line|
+            encoded_line = line.encode @@output_encoding
+            if encoded_line.match /^\d/
+                @@lines << encoded_line
+                lasts = load_sets(encoded_line, lasts) 
+            end
+        end
+    end
+
+
+    def load_sets(line, lasts)
+        elements = line.split /\|/
+
+        lasts[0] = load_if_different(@@zipcodes         ,[elements[0], elements[5]], lasts[0])
+        lasts[1] = load_if_different(@@settlement_types , elements[2]              , lasts[1])
+        lasts[2] = load_if_different(@@municipalities   ,[elements[3], elements[4]], lasts[2])
+        lasts[3] = load_if_different(@@states           , elements[4]              , lasts[3])
+        lasts[4] = load_if_different(@@cities           , elements[5]              , lasts[4])
+        
+        @@settlements << [elements[1], elements[0], elements[2], elements[3], elements[5]]
+
+        lasts
+    end
+
+    def load_if_different(set, element, last)
+        if element != last and element.size > 0
+            set << element 
+            last = element
+        end
+        last
+    end
+
+    def print_summary_info
         puts "#{@@settlements.size} asentamientos"
         puts "#{@@zipcodes.size} códigos postales"
         puts "#{@@settlement_types.size} tipos de asentamiento"
         puts "#{@@municipalities.size} municipios"
         puts "#{@@cities.size} ciudades"
         puts "#{@@states.size} estados"
+    end
 
+    def write_output_file
         reset_file @@sql_file_name
 
         build_simple_table('estado', @@states, @@states_map)
         build_simple_table('tipo_asentamiento', @@settlement_types, @@settlement_types_map)
         build_simple_table('ciudad', @@cities, @@cities_map)
 
-        build_child_table('codigo_postal', @@zipcodes, @@zipcodes_map, 'ciudad', @@cities_map)
         build_child_table('municipio', @@municipalities, @@municipalities_map, 'estado', @@states_map)
-
-        build_settlement_table
         
+        build_settlement_table
+    end
+
+    def reset_file filename
+        File.delete(filename) if File.exist? filename
+        File.open(filename, 'w') do |file|
+            file << "DROP SCHEMA IF EXISTS #{@@sql_schema};\n"
+            file << "CREATE SCHEMA IF NOT EXISTS #{@@sql_schema} DEFAULT CHARACTER SET utf8;\n"
+        end
     end
 
     def build_settlement_table
@@ -77,25 +135,30 @@ module Zipcodes
 CREATE TABLE #{@@sql_schema}.asentamiento 
 (id INT PRIMARY KEY,
 name VARCHAR(#{max_len}),
-fk_codigo_postal INT NULL,
+codigo_postal VARCHAR(5),
 fk_tipo_asentamiento INT NULL ,
 fk_municipio INT NULL, 
-CONSTRAINT FOREIGN KEY (fk_codigo_postal) REFERENCES codigo_postal (id),
+fk_ciudad INT NULL, 
 CONSTRAINT FOREIGN KEY (fk_tipo_asentamiento) REFERENCES tipo_asentamiento (id),
-CONSTRAINT FOREIGN KEY (fk_municipio) REFERENCES municipio (id)
+CONSTRAINT FOREIGN KEY (fk_municipio) REFERENCES municipio (id),
+CONSTRAINT FOREIGN KEY (fk_ciudad) REFERENCES ciudad (id)
 ) ENGINE = InnoDB;
+
+CREATE INDEX cp_idx ON #{@@sql_schema}.asentamiento (codigo_postal);
 
 END_HERE
 
             @@settlements.each_with_index do |element, index|
 
                 asentamiento = element[0]
-                fk_cp = @@zipcodes_map[element[1]]
+                cp = element[1]
                 fk_st = @@settlement_types_map[element[2]]
                 fk_mu = @@municipalities_map[element[3]]
+                fk_ci= @@cities_map[element[4]] || "NULL"
+                
 
                 
-                file << "INSERT INTO #{@@sql_schema}.asentamiento VALUES (#{index + 1}, '#{asentamiento}', #{fk_cp}, #{fk_st}, #{fk_mu});\n"
+                file << "INSERT INTO #{@@sql_schema}.asentamiento VALUES (#{index + 1}, '#{asentamiento}', #{cp}, #{fk_st}, #{fk_mu}, #{fk_ci});\n"
             end
              
         end
@@ -135,51 +198,7 @@ END_HERE
 
     end
 
-    def file_iterator file
-        puts "Loading file #{file}"
-        File.open(file,"r:#{@@input_encoding}")
-    end
 
-    def read_lines file
-        lasts = ['','','','','']
-        file.each_line do |line|
-            encoded_line = line.encode @@output_encoding
-            if encoded_line.match /^\d/
-                @@lines << encoded_line
-                lasts = load_sets(encoded_line, lasts) 
-            end
-        end
-    end
-
-    def reset_file filename
-        File.delete(filename) if File.exist? filename
-        File.open(filename, 'w') do |file|
-            file << "DROP SCHEMA IF EXISTS #{@@sql_schema};\n"
-            file << "CREATE SCHEMA IF NOT EXISTS #{@@sql_schema} DEFAULT CHARACTER SET utf8;\n"
-        end
-    end
-
-    def load_sets(line, lasts)
-        elements = line.split /\|/
-
-        lasts[0] = load_if_different(@@zipcodes         ,[elements[0], elements[5]], lasts[0])
-        lasts[1] = load_if_different(@@settlement_types , elements[2]              , lasts[1])
-        lasts[2] = load_if_different(@@municipalities   ,[elements[3], elements[4]], lasts[2])
-        lasts[3] = load_if_different(@@states           , elements[4]              , lasts[3])
-        lasts[4] = load_if_different(@@cities           , elements[5]              , lasts[4])
-        
-        @@settlements << [elements[1], elements[0], elements[2], elements[3]]
-
-        lasts
-    end
-
-    def load_if_different(set, element, last)
-        if element != last and element.size > 0
-            set << element 
-            last = element
-        end
-        last
-    end
 
     def build_simple_table(table_name, set, map)
 
